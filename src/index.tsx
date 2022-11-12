1687e8e99e00061bf7bfdd45e12a4dc74da3ca5f
hypensterisk <roykhanalaushaf0@gmail.com>
2022-11-12 14:14:06 +0700

    update(src/index.tsx): render <App/> to container

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,16 @@
 /** @format */
 
 import './index.css'
+import {createRoot} from 'react-dom/client'
+import {StrictMode} from 'react'
+import App from './app'
+
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+const children = (
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
+
+root.render(children)
