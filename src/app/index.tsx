070f626343e33e80a23b0a5845680aa193db30d8
hypensterisk <roykhanalaushaf0@gmail.com>
2022-11-16 16:29:14 +0700

    update(app/index.tsx): add main section

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,20 @@
 /** @format */
 
 import Header from './header'
+import Main from './main'
+import {Suspense} from 'react'
+import Spinner from './Spinner'
+import {Routes} from 'react-router-dom'
 
 export default function App() {
   return (
     <>
       <Header />
+      <Main>
+        <Suspense fallback={<Spinner />}>
+          <Routes></Routes>
+        </Suspense>
+      </Main>
     </>
   )
 }
